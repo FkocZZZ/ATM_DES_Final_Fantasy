0f7ca01c188c02ad6e381d04847ff821746832ce
Kim Phú <PHU.HHK05004@sinhvien.hoasen.edu.vn>
2024-10-16 22:48:02 +0700

    Delete server directory

--- server/routes/postTransactionRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { postTransaction } = require('../controllers/postTransactionController');
-
-
-
-router.route('/postTransaction').post(postTransaction);
-
-module.exports = router;
